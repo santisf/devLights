4d0274b5346b645c671de484404245463200e56a
Santiago Foti <santiago.foti1@gmail.com>
2022-11-25 21:10:24 -0300

    Primer tarea

---
+++ devLights.js
@@ -0,0 +1,533 @@
+//Ejercicio 1 de FreeCodeCamp ------------------------------------------------------
+
+/* "// utilizado para comentarios simples de una sola linea"
+
+    "/*" al principio del comentario y revetir el orden de los simbolos al final para comentarios
+    de más de una linea
+*/
+
+
+//Ejercicio 2 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Use la varpalabra clave para crear una variable llamada myName.
+
+var myName;
+
+
+//Ejercicio 3 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Asigne el valor 7 a la variable a.
+
+var a;
+a = 7;
+
+
+//Ejercicio 4 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Asigne el contenido de a a la variable b.
+
+var a;
+a = 7;
+var b;
+b = a;
+
+
+//Ejercicio 5 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Debe inicializar a con un valor de 9.
+
+var a = 9;
+
+
+//Ejercicio 6 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cree dos nuevas variables de cadena: myFirstName y myLastName asígneles 
+//los valores de su nombre y apellido, respectivamente.
+
+var myFirstName= "Santiago";
+var myLastName= "Foti";
+
+
+//Ejercicio 7 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Inicialice las tres variables a, b, y ccon 5, 10 y 
+//"I am a"respectivamente para que no sean undefined.
+
+var a= 5;
+var b=10;
+var c="I am a";
+
+
+//Ejercicio 8 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Modifique las declaraciones y asignaciones existentes para que sus nombres usen camelCase .
+//No cree ninguna variable nueva.
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+//Ejercicio 9 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Actualice el código para que solo use la let como palabra clave.
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+
+//Ejercicio 10 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie el código para que todas las variables se declaren usando leto const. 
+//Úselo letcuando desee que la variable cambie y constcuando desee que la variable permanezca constante. 
+//Además, cambie el nombre de las variables declaradas con constpara ajustarse a las prácticas comunes. 
+//No cambie las cadenas asignadas a las variables.
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+//Ejercicio 11 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie el 0 para que la suma sea igual a 20.
+
+const sum = 10 + 10;
+
+
+//Ejercicio 12 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie el 0para que la diferencia sea 12.
+
+const difference = 45 - 33;
+
+
+//Ejercicio 13 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie el 0 para que el producto sea igual a 80.
+
+const product = 8 * 10;
+
+
+//Ejercicio 14 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambia el 0 para que la division sea igual a 2.
+
+const quotient = 66 / 33;
+
+
+//Ejercicio 15 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie el código para usar el ++operador en myVar.
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+
+//Ejercicio 16 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie el código para usar el --operador en myVar.
+
+let myVar2 = 11;
+
+// Only change code below this line
+myVar2--;
+
+
+//Ejercicio 17 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Crea una variable myDecimaly 
+//dale un valor decimal con una parte fraccionaria (por ejemplo, 5.7)
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 7.3
+
+
+//Ejercicio 18 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie el 0.0 para que el producto sea igual a 5.0
+
+const product1 = 2.0 * 2.5;
+
+
+//Ejercicio 20 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie el 0.0para que quotient sea igual a 2.2.
+
+const quotient1 = 4.4 / 2.0; // Change this line
+
+
+
+//Ejercicio 21 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Establecer remainder igual al resto de 11 dividido 3 usando el operador de resto ( ).%
+
+const remainder = 11 % 3;
+
+
+//Ejercicio 22 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Convierta las asignaciones para a, b y c para usar el += operador.
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+
+//Ejercicio 23 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Convierta las asignaciones para a, b y c para usar el -= operador.
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+//Ejercicio 24 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Convierta las asignaciones para a, b y c para usar el *= operador.
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3 ;
+c *= 10;
+
+
+
+//Ejercicio 25 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Convierta las asignaciones para a, by c para usar el /= operador.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+
+//Ejercicio 26 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Use barras diagonales inversas para asignar una cadena a la myStrvariable, 
+//de modo que si la imprimiera en la consola, vería:
+//I am a "double quoted" string inside "double quotes".
+
+const myStr2 = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+
+//Ejercicio 27 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cambie la cadena proporcionada a una cadena con comillas simples al principio y al final y sin caracteres de escape.
+//En este momento, la <a>etiqueta en la cadena usa comillas dobles en todas partes. 
+//Deberá cambiar las comillas externas por comillas simples para poder eliminar los caracteres de escape.
+
+const myStr3 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+//Ejercicio 28 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Asigne las siguientes tres líneas de texto a la única variable myStrusando secuencias de escape.
+/*
+Primera Línea
+\
+Segunda Línea Tercera Línea
+*/
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+//Ejercicio 29 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Compile myStr a partir de las cadenas This is the start. y This is the end. use el + operador. 
+//Asegúrese de incluir un espacio entre las dos cadenas.
+
+const myStr8 = "This is the start."+" "+"This is the end."; // Change this line
+
+
+//Ejercicio 30 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Construya myStrsobre varias líneas concatenando estas dos cadenas: 
+//This is the first sentence. y This is the second sentence. usando el += operador.
+//Utilice el += operador de forma similar a como se muestra en el ejemplo y asegúrese de incluir un espacio entre las dos cadenas. 
+//Comience asignando la primera cadena a myStr, luego agregue la segunda cadena.
+
+let myStr7;
+
+myStr = "This is the first sentence.";
+
+myStr+=" "+"This is the second sentence."
+
+
+//Ejercicio 31 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Establézca myName en una cadena igual a su nombre y 
+//construya myStr entre myName las cadenas My name is y and I am well!
+
+// Only change code below this line
+const myName = "Santiago";
+const myStr4 = "My name is "+myName+" and I am well!";
+
+
+//Ejercicio 32 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Establézca someAdjective en una cadena de al menos 3 caracteres y agréguelo al myStr con +=.
+
+// Change code below this line
+const someAdjective = "fun";
+let myStr6 = "Learning to code is ";
+
+myStr+=someAdjective;
+
+
+//Ejercicio 33 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Utilice .length  para establecer lastNameLength con el número de caracteres en lastName.
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+
+//Ejercicio 34 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Utilice la notación de corchetes para encontrar el primer carácter de la lastNamevariable
+// y asígnelo a firstLetterOfLastName.
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName1 = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName1[0]; // Change this line
+
+
+//Ejercicio 35 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Corrija la asignación para myStrque contenga el valor de cadena del Hello World
+//uso del enfoque que se muestra en el ejemplo anterior.
+
+// Setup
+let myStr5 = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+
+//Ejercicio 36 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Igualar thirdLetterOfLastName a la tercera letra de lastNamevariable 
+//usando la notación de paréntesis.
+
+// Setup
+const lastName4 = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+
+//Ejercicio 37 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Utilice la notación de corchetes para encontrar el último carácter de la lastNamevariable.
+
+// Setup
+const lastName2 = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+
+
+//Ejercicio 38 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Use la notación de corchetes para encontrar el penúltimo carácter en la lastNamecadena.
+
+// Setup
+const lastName3 = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+
+
+//Ejercicio 39 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: insertar las variables en una oracion con el operador +
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My "+myAdjective+" "+myNoun+" "+myVerb+" very "+myAdverb; // Change this line
+// Only change code above this line
+
+
+
+//Ejercicio 40 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Modifique la nueva matriz myArray para que contenga tanto una cadena como un número (en ese orden).
+
+// Only change code below this line
+const myArray = ["stock", 2];
+
+
+
+//Ejercicio 41 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cree un array anidado llamado myArray.
+
+// Only change code below this line
+const myArray1 = [["puntosAFavor",2], ["puntosEnContra", 4]];
+
+
+//Ejercicio 42 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cree una variable llamada myDatay configúrela para que sea igual 
+//al primer valor del myArrayuso de la notación de corchetes.
+
+const myArray2 = [50, 60, 70];
+
+let myData = myArray2[0];
+
+
+//Ejercicio 43 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Modifique los datos almacenados en el índice 0 de myArray a 45.
+
+// Setup
+const myArray3 = [18, 64, 99];
+
+// Only change code below this line
+myArray3[0]=45;
+
+
+//Ejercicio 44 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: seleccione un elemento de myArraytal que myDatasea igual a 8.
+
+const myArray4 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+  ];
+  
+  const myData1 = myArray[2][1];
+
+
+//Ejercicio 45 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Empuje ["dog", 3]hacia el final de myArray.
+
+// Setup
+const myArray5 = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray5.push(["dog", 3])
+
+
+//Ejercicio 46 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Utilice .pop() para eliminar el último elemento myArray
+//y asignar el valor extraído a una nueva variable, removedFromMyArray.
+
+// Setup
+const myArray6 = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+let removedFromMyArray= myArray6.pop();
+
+
+
+//Ejercicio 47 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Utilice .shift() para eliminar el primer elemento de myArray
+//y asignar el valor "desplazado" a una nueva variable, removedFromMyArray.
+
+// Setup
+const myArray7 = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+let removedFromMyArray1 = myArray7.shift();
+
+
+//Ejercicio 48 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Agregue ["Paul", 35] al comienzo de  myArray8 usando unshift().
+
+// Setup
+const myArray8 = [["John", 23], ["dog", 3]];
+myArray8.shift();
+
+// Only change code below this line
+myArray8.unshift(["Paul", 35])
+
+
+//Ejercicio 49 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: haz una lista de compras con arrays anidados con el producto y la cantidad
+
+const myList = [["harina", 3],["aceite",2],["azucar",2],["yerba",1],["cereal",0]];
+
+
+
+//Ejercicio 50 de FreeCodeCamp-------------------------------------------------------
+
+//CONSIGNA: Cree una función llamada reusableFunctionque imprima la cadena Hi Worlden la consola de desarrollo.
+//Llame a la función.
+
+function reusableFunction(){
+
+    console.log("Hi World");
+  
+  };
+  
+  reusableFunction();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
